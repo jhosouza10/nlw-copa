007db7ceadb5fd2c9868cb0ee75593cb416f3025
Jhonatan Ferreira <=>
2022-11-04 22:23:55 -0300

    Adicionando novos jogos no javascript

--- js/main.js
+++ js/main.js
@@ -22,11 +22,7 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-  createCard(
-    "28/11",
-    "segunda",
-    createGame("switzerland", "13:00", "brazil") +
-      createGame("portugal", "13:00", "uruguay")
-  ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia") + createGame("switzerland", "07:00", "cameroon")) +
+  createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil") + createGame("portugal", "13:00", "uruguay")) +
+  createCard("01/12", "quinta", createGame("brazil", "16:00", "cameroon") + createGame("serbia", "16:00", "switzerland")) + 
+  createCard("02/12", "sexta", createGame("hungary", "16:00", "japan") + createGame("colombia", "16:00", "switzerland"))
